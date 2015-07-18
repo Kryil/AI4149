7dac454d5e676e32057928983cfce4e1c2f6245a
Verna Koskinen <darth.kipsu@gmail.com>
2015-07-18 23:52:26 +0300

    use background image for gamefield

--- webreplay/resources/public/script/canvas.js
+++ webreplay/resources/public/script/canvas.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     var canvas = document.getElementById("gamefield");
     var ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "#2ecc71",
+    ctx.fillStyle = "#95a5a6",
     ctx.fillRect(0, 0, 500, 500);
+
+    var background = new Image();
+    background.onload = function() {
+        ctx.drawImage(background, 0, 0);
+    }
+    background.src = "images/metal-tileable.png";
 });
