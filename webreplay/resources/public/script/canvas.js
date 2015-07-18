332ded8756b8d1142a464f3415cf96c9579ec424
Verna Koskinen <darth.kipsu@gmail.com>
2015-07-19 01:39:33 +0300

    draw some walls

--- webreplay/resources/public/script/canvas.js
+++ webreplay/resources/public/script/canvas.js
@@ -1,23 +1,39 @@
 $(document).ready(function() {
     var canvas = document.getElementById("gamefield");
-    var ctx = canvas.getContext("2d");
 
-    gameField.draw.initial(ctx, "images/metal-tileable.png");
+    gameField.draw.initial(canvas, "images/metal-tileable.png");
+
+    $.getJSON("script/dummydata.json", function(data) {
+        gameField.draw.fromJSON(data);
+    });
 });
 
 var gameField = {};
 
 gameField.draw = (function() {
     var background = new Image();
+    var canvas;
+    var ctx;
 
-    function initialize(ctx, imageUrl) {
-        background.onload = function() {
-            ctx.drawImage(background, 0, 0);
-        };
+    function initialize(newCanvas, imageUrl) {
+        canvas = newCanvas;
+        ctx = canvas.getContext("2d");
         background.src = imageUrl;
+        ctx.drawImage(background, 0, 0);
+    };
+
+    function drawFromJSON(data) {
+        ctx.drawImage(background, 0, 0);
+        ctx.scale(canvas.width / data.gamefield.size[0],
+                  canvas.height / data.gamefield.size[1]);
+
+        data.gamefield.walls.forEach(function(el, i, array) {
+            ctx.fillRect(el[1], el[0], 1, 1);
+        });
     };
 
     return {
-        initial: initialize
+        initial: initialize,
+        fromJSON: drawFromJSON
     };
 })();
