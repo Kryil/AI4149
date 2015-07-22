0e1b5992af337810aeed665f5e65fbe41b6a4f91
Verna Koskinen <darth.kipsu@gmail.com>
2015-07-22 09:28:56 +0300

    move initialization of canvas drawing to script.js

--- webreplay/resources/public/script/canvas.js
+++ webreplay/resources/public/script/canvas.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-    var canvas = document.getElementById("gamefield");
-
-    gameField.draw.initial(canvas, "images/metal-tileable.png");
-
-    $.getJSON("script/dummydata.json", function(data) {
-        gameField.draw.fromJSON(data);
-    });
-});
-
 var gameField = {};
 
 gamefield.drawList = (function() {
---
+++ webreplay/resources/public/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var canvas = document.getElementById("gamefield");
+
+    gameField.draw.initial(canvas, "images/metal-tileable.png");
+
+    $.getJSON("script/dummydata.json", function(data) {
+        gameField.draw.fromJSON(data);
+    });
+});
