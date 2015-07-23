6eaf3d2c998e31d10a14cb276ae99483afabc5ad
Ilari Mäkimattila <ilari.makimattila@kompozure.com>
2015-07-23 17:51:23 +0200

    Iterate through dummy data

--- webreplay/project.clj
+++ webreplay/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [http-kit "2.1.18"]]
--- webreplay/src/webreplay/communication.clj
+++ webreplay/src/webreplay/communication.clj
@@ -1,12 +1,34 @@
 (ns webreplay.communication
   (use [org.httpkit.server :only [send!]]))
 
-
 (def game-channels (atom {}))
 (def game-ids (atom {}))
 
-(defn dummydata [i]
-  (slurp (str "resources/public/script/dummydata" i ".json")))
+
+(defn- loop-while [loop-fn]
+  (let [lh (fn lh [iteration]
+              (when (loop-fn iteration)
+                (println (str "loop-fn returned truthy on iteration " iteration))
+                (future (lh (inc iteration)))
+              nil))]
+    (lh 0)))
+
+(defn- dummydata [i]
+  (try
+    (slurp (str "resources/public/script/dummydata" i ".json"))
+    (catch Exception e nil)))
+
+(defn- replay [channel game-id] 
+  (println (str "replaying game " game-id))
+  (loop-while (fn [iteration]
+                (println (str game-id " iteration " iteration))
+                (if (= game-id "dummygame")
+                  (when-let [data (dummydata iteration)]
+                    (do 
+                      (send! channel data)
+                      (Thread/sleep 1000)
+                      true))
+                  nil)))) ; todo get replay from storage
 
 (defn unsubscribe [channel status]
   (let [game-id (get @game-ids channel)]
@@ -15,5 +37,6 @@
 (defn subscribe [channel game-id]
   (let [id (keyword game-id)]
     (swap! game-ids assoc channel id)
-    (send! channel (dummydata 0))))
+    (replay channel game-id)))
+
 
