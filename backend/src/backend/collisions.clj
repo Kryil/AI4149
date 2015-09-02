f042dd62d64a317e8024791373655aae0a8d0f63
Ilari Mäkimattila <ilari.makimattila@kompozure.com>
2015-09-02 22:58:07 +0200

    Add more collision helpers

--- backend/src/backend/collisions.clj
+++ backend/src/backend/collisions.clj
@@ -1,19 +1,47 @@
 (ns backend.collisions
-  (:use [backend.messages])
+  (:use [backend.messages]
+        [backend.helpers])
   (:import [backend.messages Coordinates]))
 
 (defn shape->area [shape origo]
   (mapv (fn [[x y]] [(+ (:x origo) x) (+ (:y origo) y)]) shape))
 
-(defn intersects? 
-  "Tests are coordinates inside area. Currently supports only rectangular areas."
-  [area coords]
+(defn get-unit-area [unit unit-rules]
+  (let [unit-rule (find-unit-rule (:type unit) unit-rules)]
+    (shape->area (:shape unit-rule) (:position unit))))
+
+
+(defn point-intersects? 
+  "Tests is the point inside area. Currently supports only rectangular
+  areas."
+  [area point]
   (let [sorted-area (sort area)
         min-point (first sorted-area)
         max-point (last sorted-area)
-        x (:x coords)
-        y (:y coords)]
+        x (first point)
+        y (second point)]
     (and (>= x (first min-point))
          (>= y (second min-point))
          (<= x (first max-point))
          (<= y (second max-point)))))
+
+(defn coord-intersects?
+  "Tests are coordinates inside area. Currently supports only rectangular
+  areas."
+  [area coords]
+  (point-intersects? area [(:x coords) (:y coords)]))
+ 
+(defn area-intersects?
+  "Tests does the two areas intersect each other. Currently supports only 
+  rectangular areas."
+  [area-a area-b]
+  (let [sorted-area-a (sort area-a)
+        sorted-area-b (sort area-b)
+        min-point-a (first sorted-area-a)
+        max-point-a (last sorted-area-a)
+        min-point-b (first sorted-area-b)
+        max-point-b (last sorted-area-b)]
+    (or (point-intersects? sorted-area-a min-point-b)
+        (point-intersects? sorted-area-a max-point-b)
+        (point-intersects? sorted-area-b min-point-a)
+        (point-intersects? sorted-area-b max-point-a))))
--- backend/test/backend/collision_test.clj
+++ backend/test/backend/collision_test.clj
@@ -10,14 +10,26 @@
     (fact "coordinates and shape is converted to area"
       (collisions/shape->area rectangle-shape origo) => [[60 45] [60 55] [40 55] [40 45]])))
 
-(facts "intersects"
+(facts "coord intersects"
   (fact "coordinates intersect with object"
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[50 50]) => truthy
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 55]) => truthy
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 50]) => truthy)
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[50 50]) => truthy
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 55]) => truthy
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 50]) => truthy)
   (fact "coordinates do not intersect with object"
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[10 10]) => falsey
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 0]) => falsey
-    (collisions/intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[61 50]) => falsey))
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[10 10]) => falsey
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[60 0]) => falsey
+    (collisions/coord-intersects? [[60 45] [60 55] [40 55] [40 45]] #backend.messages.Coordinates[61 50]) => falsey))
+
+(facts "area intersects"
+  (let [area [[60 45] [60 55] [40 55] [40 45]]]
+    (fact "areas intersect with each other"
+      (collisions/area-intersects? area area) => truthy
+      (collisions/area-intersects? area [[55 50] [55 55] [50 55] [50 50]]) => truthy
+      (collisions/area-intersects? area [[50 50] [51 51] [50 51] [51 50]]) => truthy)
+    (fact "areas do not intersect with each other"
+      (collisions/area-intersects? area [[600 450] [600 550] [400 550] [400 450]]) => falsey
+      (collisions/area-intersects? area [[60 56] [60 60] [40 60] [40 56]]) => falsey
+      (collisions/area-intersects? area [[61 45] [61 55] [70 55] [70 45]]) => falsey)))
  
+
 
