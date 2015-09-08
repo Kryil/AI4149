e722ee35d9339eb1735fc2c87b9e2ff6862bf040
Ilari Mäkimattila <ilari.makimattila@kompozure.com>
2015-09-08 21:51:31 +0200

    Fix val-id bug and remove commented code

--- engine/src/ai4149/engine/helpers.clj
+++ engine/src/ai4149/engine/helpers.clj
@@ -57,15 +57,12 @@
                      (filter (fn [s] (not= (key-fn s) (key-fn new-state-val))) (k state))))))
 
 (defn update-state 
-  ([state k new-val] (update-state state k new-val (:id new-val)))
-  ([state k new-val val-id]
+  ([state k new-val] (update-state state k new-val :id))
+  ([state k new-val val-fn]
    (assoc state 
           k 
           (cons new-val 
-                (filter (fn [s] (not= (:id s) val-id)) (k state))))))
-  #_(assoc up-p-state :building-states 
-                          (cons up-b-state 
-                                (filter (fn [bs] (not= (:id bs) building-id)) (:building-states up-p-state))))
+                (filter (fn [s] (not= (val-fn s) (val-fn new-val))) (k state))))))
 
 (defn add-to-state 
   "Add new-val to state coll k."
