af34cc0853724347e9d2f8ee4f4f33b7a87fc9e3
Ilari Mäkimattila <ilari.makimattila@kompozure.com>
2015-09-12 13:43:40 +0200

    Game ends when a commander dies

--- engine/src/ai4149/engine/game.clj
+++ engine/src/ai4149/engine/game.clj
@@ -19,22 +19,39 @@
         [winner & others] player-values]
   (assoc state :winner [(first winner) :value (second winner) (into {} others)])))
 
+(defn- commander-dead [state]
+  (let [alive-player (first (filter (fn [[pid ps]] (some #(and (type= % :commander) 
+                                                               (not (action= % :dead)))
+                                                         (vals (:units ps))))
+                                    (:players state)))]
+  (assoc state :winner [(first alive-player) :last-man-standing])))
+
+(defn- is-a-commander-dead? [state]
+  (let [players (vals (:players state))
+        units (apply concat (map #(vals (:units %)) players))
+        commanders (filter #(type= % :commander) units)]
+    (some #(action= % :dead) commanders)))
+    
+
 (defn check-game-end [state]
   (cond 
     (> (:turn state) (:turns state)) (turns-over state)
+    (is-a-commander-dead? state) (commander-dead state)
     :else state))
 
 
 (defn process-turn 
   "Apply all player actions into given state and return updated state."
   [state player-commands]
-  (let [next-state (->
-                     state
-                     (process-units (filter (fn [cmd] (not (action= cmd :fire))) player-commands))
-                     (process-factories)
-                     (process-fire-commands (filter (fn [cmd] (action= cmd :fire)) player-commands))
-                     (move-projectiles)
-                     (process-build-commands (filter (fn [cmd] (= (:action cmd) :build)) player-commands))
-                     increase-turn-counter
-                     check-game-end)]
-    next-state))
+  (if (not (nil? (:winner state)))
+    state
+    (let [next-state (->
+                       state
+                       (process-units (filter (fn [cmd] (not (action= cmd :fire))) player-commands))
+                       (process-factories)
+                       (process-fire-commands (filter (fn [cmd] (action= cmd :fire)) player-commands))
+                       (move-projectiles)
+                       (process-build-commands (filter (fn [cmd] (= (:action cmd) :build)) player-commands))
+                       increase-turn-counter
+                       check-game-end)]
+      next-state)))
--- engine/test/ai4149/engine/game_test.clj
+++ engine/test/ai4149/engine/game_test.clj
@@ -4,7 +4,9 @@
             [ai4149.engine.helpers :refer :all]
             [ai4149.engine.collisions :as collisions]
             [ai4149.engine.game :as game]
-            [ai4149.game-test-data :refer [simple-test-state]]))
+            [ai4149.game-test-data :refer [simple-test-state]])
+  (:import [ai4149.messages PlayerCommand]
+           [ai4149.messages Coordinates]))
 
 (fact "turn counter is increased"
   (:turn (game/process-turn simple-test-state [])) => 1
@@ -16,4 +18,9 @@
     (let [reduced-turns-state (assoc simple-test-state :turns 10)
           end-state (reduce (fn [state _] (game/process-turn state [])) reduced-turns-state (range 11))]
       (:winner end-state) =not=> nil
-      (:winner end-state) => ["player-2" :value 4100 {"player-1" 4000}])))
+      (:winner end-state) => ["player-2" :value 4100 {"player-1" 4000}]))
+  (fact "a commander dies"
+    (let [nearly-dead-commander-state (assoc-in simple-test-state [:players "player-1" :units "p1-commander" :health] 10)
+          fire-command (PlayerCommand. "player-2" "p2-tank-1" :fire [:main (Coordinates. 30 12)])
+          end-state (game/process-turn nearly-dead-commander-state [fire-command])]
+      (:winner end-state) => ["player-2" :last-man-standing])))
